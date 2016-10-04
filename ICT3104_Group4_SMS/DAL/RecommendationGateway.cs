73814e47f2cefbc90af17cfd6d89a3db7607df90
meixi-l <lim.m3ixi@gmail.com>
2016-10-04 21:35:00 +0800

    completed update recommendations

---
+++ ICT3104_Group4_SMS/DAL/RecommendationGateway.cs
@@ -0,0 +1,18 @@
+﻿using ICT3104_Group4_SMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICT3104_Group4_SMS.DAL
+{
+    public class RecommendationGateway : DataGateway<Recommendation>
+    {        
+        public ICollection<Recommendation> GetRecByGradeIds(int[] gradeIds)
+        {
+            ICollection<Recommendation> model = new List<Recommendation>();
+            model = data.Where(r => gradeIds.Contains(r.gradeId)).ToList();
+            return model;
+        }
+    }
+}
