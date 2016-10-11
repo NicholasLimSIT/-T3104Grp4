aa53c468f19ef853d303edab0c399a3fbc9c9eb9
fenditan <reyfendi66@gmail.com>
2016-10-11 21:57:30 +0800

    Created UI for HOD Moderation, Admin Backup UI, and Lecturer Grade locking

--- ICT3104_Group4_SMS/Controllers/HODController.cs
+++ ICT3104_Group4_SMS/Controllers/HODController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ICT3104_Group4_SMS.DAL;
+using ICT3104_Group4_SMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,58 @@ namespace ICT3104_Group4_SMS.Controllers
 {
     public class HODController : Controller
     {
+        private SmsContext db = new SmsContext();
+        internal IDataGateway<Programme> ProgrammeGateway;
+        internal IDataGateway<Lecturer_Module> Lecturer_ModuleGateway;
+        internal IDataGateway<ApplicationUser> ApplicationUserGateway;
+        internal IDataGateway<Grade> GradesGateway;
+        internal IDataGateway<Recommendation> RecommendationGateway;
+        //internal IDataGateway<Module> ModuleGateway = new DataGateway<Module>();
+        private ModuleGateway ModuleGateway = new ModuleGateway();
+        private Lecturer_ModuleDataGateway lmDW = new Lecturer_ModuleDataGateway();
+
+        private SmsMapper smsMapper = new SmsMapper();
+
+        public HODController()
+        {
+            Lecturer_ModuleGateway = new Lecturer_ModuleDataGateway();
+            ProgrammeGateway = new ProgrammeDataGateway();
+            ApplicationUserGateway = new ApplicationUserDataGateway();
+            GradesGateway = new GradesGateway();
+            RecommendationGateway = new RecommendationGateway();
+        }
+
         // GET: HOD
         public ActionResult Index()
         {
             return View();
+         
         }
+
+        // GET: HOD/ModerateGrade
+        public ActionResult ModerateMark()
+        {
+            return View(db.Modules.ToList());
+
+        }
+
+        // GET: Grades
+        public ActionResult ModerateMarkView(int? id, String moduleName)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ViewBag.moduleId = id;
+            ViewBag.moduleName = moduleName;
+            if (moduleName == null)
+                ViewBag.moduleName = ModuleGateway.GetModuleName(id);
+
+            int id2 = id ?? default(int);
+            IEnumerable<GradeRecViewModel> gradeWithRecList = smsMapper.GradeWithRec(id2);
+            return View(gradeWithRecList);
+        }
+
     }
 }
